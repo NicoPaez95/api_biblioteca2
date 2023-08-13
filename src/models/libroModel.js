41f62c74348a947fc5286aa467631edf81ed7a24
NicoPaez95 <nicopaezcba4@gmail.com>
2023-08-13 01:41:32 -0300

    “Tests Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect("mongodb://127.0.0.1:27017/Biblioteca", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
